b534ba0f6e79806b6021cb7bbede1d283d9c13b5
Nicolas Schmid <nicolas@breiten.ch>
2021-05-17 18:08:59 +0200

    chores: add config to gitignore

--- config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or 'EC9F71CFE9D117F9EFB21EA96F88D'
-    SERVER_NAME = 'localhost:5000'
-    DB_CONNECTION_STRING = 'postgresql+psycopg2://postgres:' \
-        'ramsis@localhost:5432/ebrdb'
