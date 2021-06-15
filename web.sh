4f68e91887a572837e6f3845ffcf2020cb9ba679
Nicolas Schmid <nicolas@breiten.ch>
2021-06-15 16:31:01 +0200

    chores: add celery worker startup to web.sh

--- web.sh
+++ web.sh
@@ -3,6 +3,7 @@
 source env/bin/activate
 
 # use flask cli, custom cli commands are not available
+celery -A wsgi:celery_app worker --pool=solo --loglevel=info &
 flask run
 
 # use own cli, need to install first with 'pip install -e .'
