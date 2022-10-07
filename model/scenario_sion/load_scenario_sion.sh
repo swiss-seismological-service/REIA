14f7089ea20d61cdfff4b70a80cc9d45a8995bac
Nicolas Schmid <nicolas@breiten.ch>
2022-10-07 14:33:30 +0200

    feat: add updated vulnerability models

--- model/scenario_sion/load_scenario_sion.sh
+++ model/scenario_sion/load_scenario_sion.sh
@@ -2,15 +2,16 @@
 
 source ../../env/bin/activate
 
-# esl db drop
-# esl db init
+esl db drop
+esl db init
 
 esl exposure add exposure/Exposure_1km_v03a_CH_modalAmpl_amplAvg.xml scenario_sion
 
-# esl vulnerability add vulnerability/content_vulnerability_model_V003adjusted_BT.xml scenario_sion_content
-# esl vulnerability add vulnerability/displaced_ST_vulnerability_model_V003adjusted_mean.xml scenario_sion_displaced
-# esl vulnerability add vulnerability/fatality_vulnerability_model_V003adjusted_mean.xml scenario_sion_fatality
-# esl vulnerability add vulnerability/injury_vulnerability_model_V003adjusted_mean.xml scenario_sion_injury
-esl vulnerability add vulnerability/structural_vulnerability_model_V003adjusted_BT.xml scenario_sion_structural
+# esl vulnerability add vulnerability/content_vulnerability_model_fully_empirical_LG06_modi2022_Vmean_BT_agg1-4-20-85.xml scenario_sion_content
+# esl vulnerability add vulnerability/displaced_vulnerability_model_fully_empirical_LG06_modi2022_Vmean_agg1-4-20-85.xml scenario_sion_displaced
+# esl vulnerability add vulnerability/fatality_vulnerability_model_fully_empirical_LG06_modi2022_Vmean_agg1-4-20-85.xml scenario_sion_fatality
+# esl vulnerability add vulnerability/injury_vulnerability_model_fully_empirical_LG06_modi2022_Vmean_agg1-4-20-85.xml scenario_sion_injury
+# esl vulnerability add vulnerability/structural_vulnerability_model_fully_empirical_LG06_modi2022_Vmean_BT_agg1-4-20-85.xml scenario_sion_structural
+esl vulnerability add vulnerability/structural_vulnerability_model_V003adjusted_BT.xml scenario_sion_structural_old
 
 deactivate
