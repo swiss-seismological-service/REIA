4e91679eeb01cbba3628acadc7a34d5590f3b473
Nicolas Schmid <nicolas@breiten.ch>
2022-11-21 16:53:56 +0100

    refactor: use oqparam instead of events/weights to write weight

--- core/io/dstore.py
+++ core/io/dstore.py
@@ -35,10 +35,7 @@ def get_risk_from_dstore(dstore: DataStore, risk_type: ERiskType):
     # events have an associated weight which comes from the branch weight
     events['weight'] = events['rlz_id'].map(weights.__getitem__)
 
-    # number of ground motion fields * number of branches
-    num_events = len(events)
-
     df['weight'] = df['eventid'].map(
-        events['weight']) / (num_events / len(weights))
+        events['weight']) / dstore['oqparam'].number_of_ground_motion_fields
 
     return df
