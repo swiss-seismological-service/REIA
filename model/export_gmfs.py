3de1e7973f51f090072bdf5384ae91e722fa729b
Nicolas Schmid <nicolas@breiten.ch>
2023-02-28 16:51:34 +0100

    fix: export script for gmfs to write correct column name

--- model/export_gmfs.py
+++ model/export_gmfs.py
@@ -2,7 +2,7 @@
 from openquake.commonlib.datastore import read
 
 # pd.set_option('display.max_rows', None)
-dstore = read('calc_11.hdf5')
+dstore = read('test_model/calc_27257.hdf5')
 
 oq_parameter_inputs = dstore['oqparam']
 all_keys = list(dstore.keys())
@@ -12,12 +12,13 @@ all_agg_keys = [d.decode().split(',')
 
 total_values_per_agg_key = dstore.read_df('agg_values')
 
-site_collection = dstore.read_df('sitecol', 'sids')
-gmf_data = dstore.read_df('gmf_data', 'eid')
-gmf_data.rename(columns={'gmv_0': 'gmv_MMI'}, inplace=True)
+site_collection = dstore.read_df('sitecol')[['sids', 'lon', 'lat']]
+gmf_data = dstore.read_df('gmf_data')
+
+site_collection.rename(columns={'sids': 'site_id'}, inplace=True)
 
 with open('gmfs.csv', 'w') as f:
-    gmf_data.to_csv(f)
+    gmf_data.to_csv(f, index=False)
 
 with open('sites.csv', 'w') as f:
-    site_collection.to_csv(f)
+    site_collection.to_csv(f, index=False)
