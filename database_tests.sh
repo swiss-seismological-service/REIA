079d1da6e0d1569d80c0cd20d51eb725141addab
Nicolas Schmid <nicolas@breiten.ch>
2023-08-31 09:16:58 +0200

    fix: small fixes and tests

--- database_tests.sh
+++ database_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 docker build -t reia_db .
-docker run --env-file=.env -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -p 5432:5432 reia_db
+docker run --env-file=.env -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -p 5432:5432 -d reia_db
 
 source env/bin/activate
 reia exposure add reia/db/tests/data/exposure_mim_rb.xml test
