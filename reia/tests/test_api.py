d9e2fa9dee55768fa394b93a1cb14bffb85e4321
Nicolas Schmid <nicolas@breiten.ch>
2023-09-05 11:18:54 +0200

    tests: improve test setup, fix dependency

--- reia/tests/test_api.py
+++ reia/tests/test_api.py
@@ -1,32 +1,37 @@
 import time
 from io import StringIO
+from pathlib import Path
 
 from reia.api import OQCalculationAPI
 from settings import get_config
 
 
 def test_api():
-    with open('reia/tests/data/oq_test/exposure_model.xml', 'r') as f:
+    datafolder = Path(__file__).parent / 'data' / 'oq_test'
+
+    with open(datafolder / 'exposure_model.xml', 'r') as f:
         exposure = StringIO(f.read())
         exposure.name = 'exposure_model.xml'
-    with open('reia/tests/data/oq_test/gmf_scenario.csv', 'r') as f:
+    with open(datafolder / 'gmf_scenario.csv', 'r') as f:
         gmf_scenario = StringIO(f.read())
         gmf_scenario.name = 'gmf_scenario.csv'
-    with open('reia/tests/data/oq_test/job_risk.ini', 'r') as f:
+    with open(datafolder / 'job_risk.ini', 'r') as f:
         job_risk = StringIO(f.read())
         job_risk.name = 'job.ini'
-    with open('reia/tests/data/oq_test/sites.csv', 'r') as f:
+    with open(datafolder / 'sites.csv', 'r') as f:
         sites = StringIO(f.read())
         sites.name = 'sites.csv'
-    with open('reia/tests/data/oq_test/vulnerability.xml', 'r') as f:
+    with open(datafolder / 'vulnerability.xml', 'r') as f:
         vulnerability = StringIO(f.read())
         vulnerability.name = 'vulnerability.xml'
 
     api = OQCalculationAPI(get_config())
     api.add_calc_files(exposure, gmf_scenario, job_risk, sites, vulnerability)
     api.run()
+
     while api.status not in ['complete', 'aborted', 'failed']:
         print(api.status)
         time.sleep(1)
+
     print(api.status)
     print(api.get_result())
