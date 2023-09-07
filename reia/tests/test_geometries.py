218a140f7fb86f234aed405bf1b5dd38547afec1
Nicolas Schmid <nicolas@breiten.ch>
2023-09-07 15:12:25 +0200

    feat: add function to add and delete AggregationGeometries from Exposure Models

--- reia/tests/test_geometries.py
+++ reia/tests/test_geometries.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 import pytest
 
-from reia.cli import add_exposure, add_exposure_geometries
+from reia.cli import (add_exposure, add_exposure_geometries,
+                      delete_exposure_geometries)
 from reia.db import crud
 
 DATAFOLDER = Path(__file__).parent / 'data'
@@ -13,18 +14,33 @@ def exposure_with_geoms(db_session):
     exposure_id = add_exposure(DATAFOLDER / 'ria_test'
                                / 'exposure_test.xml', 'test')
 
-    add_exposure_geometries(exposure_id, DATAFOLDER
+    add_exposure_geometries(exposure_id, 'CantonGemeinde', DATAFOLDER
                             / 'geometries' / 'municipalities.shp')
     return exposure_id
 
 
 def test_geometries(exposure_with_geoms, db_session):
     exposure = crud.read_asset_collection(exposure_with_geoms, db_session)
+
     assert len(exposure.aggregationgeometries) == 3
+
     aggregationtags = exposure.aggregationtags
     churwalden = next((a for a in aggregationtags if a.name == 'GR3911'), None)
+
     assert churwalden is not None
     assert len(churwalden.geometries) == 1
+
     geometry = churwalden.geometries[0]
-    assert geometry.geometry.geometry_type == 'MULTIPOLYGON'
+
     assert geometry.name == 'Churwalden'
+
+    nendaz = next(
+        (a for a in exposure.aggregationgeometries if a.name == 'Nendaz'),
+        None)
+    assert nendaz is not None
+
+
+def test_geometry_deletion(exposure_with_geoms, db_session):
+    delete_exposure_geometries(exposure_with_geoms, 'CantonGemeinde')
+    exposure = crud.read_asset_collection(exposure_with_geoms, db_session)
+    assert len(exposure.aggregationgeometries) == 0
