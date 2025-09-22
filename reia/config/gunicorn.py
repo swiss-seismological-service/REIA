147f9301eb85c5ebc09ccde0f2f29369f629c4f0
Nicolas Schmid <nicolas@breiten.ch>
2025-09-22 14:33:48 +0200

    fix: improve some configuration parameters

--- reia/config/gunicorn.py
+++ reia/config/gunicorn.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import multiprocessing
 import os
 from distutils.util import strtobool
 
@@ -8,7 +7,7 @@ bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 accesslog = "-"
 access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
 
-workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
-threads = int(os.getenv("PYTHON_MAX_THREADS", 2))
+workers = int(os.getenv("WEB_CONCURRENCY", 4))
+threads = int(os.getenv("WEB_THREADS", 2))
 timeout = 600
 reload = bool(strtobool(os.getenv("WEB_RELOAD", "false")))
